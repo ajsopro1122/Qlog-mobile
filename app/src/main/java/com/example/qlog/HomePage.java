e19bdaf278dd69db9b1cba35cc17ecf9b7730a8d
ajsopro1122 <71369722+ajsopro1122@users.noreply.github.com>
2021-10-14 16:02:48 +0800

    Home Page and Walkin

--- app/src/main/java/com/example/qlog/HomePage.java
+++ app/src/main/java/com/example/qlog/HomePage.java
@@ -2,7 +2,12 @@ package com.example.qlog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Button btn1 = (Button)findViewById(R.id.walkin);
+        Button btn2 = (Button)findViewById(R.id.bookAppointment);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent int1 = new Intent(HomePage.this, WalkIn.class);
+                startActivity(int1);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent int2 = new Intent(HomePage.this,BookAppointment.class);
+                startActivity(int2);
+            }
+        });
     }
 }
